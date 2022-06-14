e4cf24220929918f7a6fa435e87f0fb30383a851
L.aconfident <100128754+Lconfident@users.noreply.github.com>
2022-06-14 15:18:28 +0800

    update

--- tiddlers/$__plugins_Gk0Wk_echarts_addons_oflg_CalendarHeatmap.js
+++ tiddlers/$__plugins_Gk0Wk_echarts_addons_oflg_CalendarHeatmap.js
@@ -1,19 +1,53 @@
-exports.onMount = function (echart) { const state = {}; return state; };
-
-exports.shouldUpdate = function (_, changedTiddlers) { return $tw.utils.count(changedTiddlers) > 0; };
-
-exports.onUpdate = function (myChart, state, addonAttributes) { const year = Number(addonAttributes.year) || new Date().getFullYear();
-
-function getData(year) { const startDate = +echarts.number.parseDate(year + '-01-01'); const endDate = +echarts.number.parseDate(+year + 1 + '-01-01');
-
-const dayTime = 3600 * 24 * 1000;
-
-const data = [];
-
-for (let time = startDate; time < endDate; time += dayTime) { const twtime = echarts.format.formatTime('yyyy-MM-dd', time).replace(/-/g, ); data.push([ echarts.format.formatTime('yyyy-MM-dd', time), $tw.wiki.filterTiddlers( '[all[tiddlers]!is[shadow]!prefix[$:/state/]!prefix[$:/temp/]sameday:created[' + twtime + ']][all[tiddlers]!is[shadow]!prefix[$:/state/]!prefix[$:/temp/]sameday:modified[' + twtime + ']]+[count[]]', ), ]); } return data; } const option = { tooltip: { position: 'top', formatter: function (p) { const format = echarts.format.formatTime('yyyy-MM-dd', p.data[0]); return format + ': ' + p.data[1]; }, }, visualMap: { min: 0, max: 10, calculable: true, orient: 'vertical', left: 0, top: 10, }, calendar: { left: 90, cellSize: 'auto', top: 20, orient: 'horizontal', range: year, dayLabel: { margin: 5, }, }, series: { type: 'heatmap', coordinateSystem: 'calendar', calendarIndex: 0, data: getData(year), }, }; option && myChart.setOption(option); myChart.on('click', 'series', function (parameters) { if (parameters.data[1] > 0) { const day = parameters.data[0].replace(/-/g, );
-
-const filter = '[all[tiddlers]!is[shadow]!prefix[$:/state/]!prefix[$:/temp/]sameday:created[' + day + ']][all[tiddlers]!is[shadow]!prefix[$:/state/]!prefix[$:/temp/]sameday:modified[' + day + ']]+[sort[]]';
-
-$tw.rootWidget.invokeActionString( '', );
-
-new $tw.Story().navigateTiddler('$:/AdvancedSearch'); } }); };
+exports.onUpdate = function (echart) {
+function getData(year) {
+  year = new Date().getFullYear();
+  var date = +echarts.number.parseDate(year + '-01-01');
+  var end = +echarts.number.parseDate(+year + 1 + '-01-01');
+  var dayTime = 3600 * 24 * 1000;
+  var data = [];
+  for (var time = date; time < end; time += dayTime) {
+    data.push([
+      echarts.format.formatTime('yyyy-MM-dd', time),
+      $tw.wiki.filterTiddlers('[sameday:modified[' + echarts.format.formatTime('yyyy-MM-dd', time).replace(/-/g, "") + ']count[]]')
+    ]);
+  }
+  return data;
+}
+  echart.setOption({
+  tooltip: {
+    position: 'top',
+    formatter: function (p) {
+      var format = echarts.format.formatTime('yyyy-MM-dd', p.data[0]);
+      return format + ': ' + p.data[1];
+    }
+  },
+  visualMap: {
+    min: 0,
+    max: 10,
+    calculable: true,
+    orient: 'vertical',
+    left: 0,
+    top: 10
+  },
+  calendar: [
+    {
+      left: 90,
+      cellSize: 'auto',
+      top: 20,
+      orient: 'horizontal',
+      range: new Date().getFullYear(),
+      dayLabel: {
+        margin: 5
+      }
+    }
+  ],
+  series: [
+    {
+      type: 'heatmap',
+      coordinateSystem: 'calendar',
+      calendarIndex: 0,
+      data: getData()
+    }
+  ]
+});
+};
